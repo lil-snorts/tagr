ee93fc5af2f7dd184877fec77653b33da87c3413
lil_snorts <taylor.nb@hotmail.com>
2024-07-14 10:47:03 +1200

    partial scripting finish. should work on Linux, workin on a windows script now

---
+++ backend/dotnet/scripts/generate.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Define variables
+GENERATOR_NAME="aspnetcore"
+CONFIG_FILE="config/openapi_generator_config.json"
+OUTPUT_PATH="../source/generated"
+
+# Create the output folde, destroying the current data stored there
+
+rm -rf $OUTPUT_PATH
+mkdir $OUTPUT_PATH
+
+# Run the OpenAPI Generator CLI command
+openapi-generator-cli generate -g $GENERATOR_NAME -c $CONFIG_FILE -o $OUTPUT_PATH
